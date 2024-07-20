c27b3745d153794f4f1c8ec0e4e3afd22159777f
Socheath-Poul <poulsocheath@gmail.com>
2024-07-20 14:26:30 +0700

    first

---
+++ app.py
@@ -0,0 +1,67 @@
+# print("Hello Student")
+#
+# print("Single 'qoute' inside double qoute")
+# print('Single "qoute" inside double qoute')
+# print("Single \'qoute\' and \"double\" qoute \ninside qoute")
+
+# name=input("What is your name?")
+# print("Welcome" + name)
+
+#Test01
+# print("==Welcome Amout Calculate==")
+#
+# qty1=int(input("Qty1 = "))
+# qty2=int(input("Qty2 = "))
+# price=float(input("Price = "))
+# total_qty=qty1+qty2
+# print("Total Qty = " + str(total_qty))
+# amount=total_qty*price
+# print("Total Amount = "+str(amount))
+
+#===Test02
+
+# a = input("a: ")
+# b = input("b: ")
+#
+# c = a
+# a = b
+# b = c
+#
+#
+# print("a: " + a)
+# print("b: " + b)
+#===Test03
+# print("Welcome to the band name generator")
+# city=input("Which city did you grow up in?\n")
+# pet=input("What is the name of the pet?\n")
+# print("Your band name could be "+ city+" " + pet)
+
+
+#20/07/2024
+#str = "Hello World!"
+# print (str)
+# print(str[0])
+# print(str[2:5])
+# print(str[2:])
+# print(str*5)
+# print(str+" From BBU ")
+# print(type(str))
+
+#Number
+a=1_000_000
+print(a)
+print(type(a))
+b=11.33
+print(b)
+print(type(b))
+
+
+val1 = int("19")
+val2 =int("24")
+
+#print(val1+val2)
+
+total=val1+val2
+print(type(total))
+print("Total = " + str(total))
+print(len(str(total)))
